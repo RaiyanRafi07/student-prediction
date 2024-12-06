9a0652be90b6885c24dc4993d7c8bfd1b824cc47
RaiyanRafi07 <77582679+RaiyanRafi07@users.noreply.github.com>
2024-12-05 23:49:36 -0500

    Update and rename DataPoint.java to Node.java

--- random_forest/src/DataPoint.java
+++
@@ -1,9 +0,0 @@
-public class DataPoint {
-    double[] features;
-    int label; // 0 for failing, 1 for passing
-
-    public DataPoint(double[] features, int label) {
-        this.features = features;
-        this.label = label;
-    }
-}
